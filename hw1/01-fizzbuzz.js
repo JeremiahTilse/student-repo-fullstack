dd03b14f578ad3da19f4052bbe619430c4ff0cf4
Jeremiah <tilse@pdx.edu>
2022-09-30 10:12:34 -0700

    Changed for loop to meet AirBnB style guidelines

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -11,7 +11,7 @@ Use console.log() to write the proper output to the command line.
 
 const fizzbuzz = () => {
   // Add your code here
-  for (let i = 1; i < 101; i++) {
+  for (let i = 1; i < 101; i += 1) {
     if (i % 5 === 0 && i % 3 === 0) console.log('fizzbuzz');
     else if (i % 3 === 0) console.log('fizz');
     else if (i % 5 === 0) console.log('buzz');
