41821c3ed016a962afeef0c6d396f0dbb1973e92
Jeremiah <tilse@pdx.edu>
2022-09-30 10:21:29 -0700

    Prettier settings change to add @format

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,13 +1,15 @@
-/** Exercise 01 - Fizzbuzz
-
-Write a program that writes all the numbers from 1 to 100, with some exceptions: 
-- For numbers divisible by 3, print “fizz” 
-- For numbers divisible by 5 (but not 3), print “buzz” 
-- For numbers divisible by 3 and 5, print “fizzbuzz”
-
-Use console.log() to write the proper output to the command line.
-
-**/
+/**
+ * Exercise 01 - Fizzbuzz
+ *
+ * Write a program that writes all the numbers from 1 to 100, with some exceptions:
+ * - For numbers divisible by 3, print “fizz”
+ * - For numbers divisible by 5 (but not 3), print “buzz”
+ * - For numbers divisible by 3 and 5, print “fizzbuzz”
+ *
+ * Use console.log() to write the proper output to the command line.
+ *
+ * @format
+ */
 
 const fizzbuzz = () => {
   // Add your code here
