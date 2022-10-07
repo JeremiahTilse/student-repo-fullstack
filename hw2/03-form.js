ce9b78166b2e0b3ef6210fc654abe8fcc517f957
Jeremiah <tilse@pdx.edu>
2022-10-07 14:29:15 -0700

    FIrst draft of 03-form, js and html

--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,55 @@
-/** Exercise 03 - Form **/
+/**
+ * Exercise 03 - Form *
+ *
+ * @format
+ */
 
 // Add your code here
+let form = document.querySelector('form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  let data = {};
+  let all = form.querySelectorAll('input');
+  let feedback = form.querySelector('textarea');
+
+  for (let field of all) {
+    if (field.type !== 'submit' && field.type !== 'reset') {
+      if (field.type == 'checkbox') {
+        if (field.checked) {
+          data[field.name] = field.value;
+        }
+      } else {
+        if (field.value) data[field.name] = field.value;
+      }
+    }
+  }
+
+  if (feedback.value) data[feedback.name] = feedback.value;
+
+  const none = 'no submission';
+
+  if (JSON.stringify(data) === '{}')
+    console.warn('You must enter some data to submit this form');
+  else {
+    console.groupCollapsed('========= Form Submission =========');
+
+    data.name ? console.log('Name:', data.name) : console.log('Name:', none);
+
+    data.email
+      ? console.log('Email:', data.email)
+      : console.log('Email:', none);
+
+    data.message
+      ? console.log('Feedback:', data.message)
+      : console.log('Feedback:', 'No feedback was submitted');
+
+    data.signUp
+      ? console.log('Newsletter:', 'Yes, I would like to join the newsletter.')
+      : console.log('Newsletter:', 'No, thank you.');
+
+    console.groupEnd();
+  }
+  event.preventDefault();
+}
