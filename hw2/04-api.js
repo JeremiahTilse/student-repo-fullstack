70a4b470d08bf70a6737f9de9643c4979b817efc
Jeremiah <tilse@pdx.edu>
2022-10-07 17:00:27 -0700

    First draft of 04-api.js

--- hw2/04-api.js
+++ hw2/04-api.js
@@ -1,7 +1,44 @@
-/** Exercise 04 - API **/
+/**
+ * Exercise 04 - API *
+ *
+ * @format
+ */
 
 const url = 'https://restcountries.com/v3.1/all';
 
 // Add your code here
+const list = document.querySelector('#results');
+const container = document.querySelector('.container');
+let div = document.createElement('div');
+div.textContent = 'Please wait while data is fetched';
+container.append(div);
+
+const addCountryToList = (item) => {
+  let element = document.createElement('li');
+
+  const { name, population } = item;
+
+  let itemString = name.common + ' - ' + population.toLocaleString('en-US');
+
+  element.textContent = itemString;
+  list.append(element);
+  div.textContent = '';
+};
+
+const getData = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(typeof data);
+      data.sort();
+      data.forEach((item) => {
+        addCountryToList(item);
+      });
+    }).catch;
+  (error) => {
+    console.log(error);
+    div.textContent = 'An error occurred. Please try again.';
+  };
+};
 
 getData(url);
