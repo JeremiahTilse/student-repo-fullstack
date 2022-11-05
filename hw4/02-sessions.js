3e355e60c8ac6883308d504cfd27b78052468652
Jeremiah <tilse@pdx.edu>
2022-11-04 19:10:28 -0700

    First draft of 02-sessions

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -1,15 +1,53 @@
+/** @format */
+
 const express = require('express');
 const session = require('express-session');
+
 const app = express();
 const port = process.env.PORT || 5001;
 
 // Add your code here
+// Generate output
+const generateOutput = (req, res, next) => {
+  let path = req.baseUrl + req.path;
+
+  let output = `Currently on route: ${path}\n\n`;
+  if (req.session.routes === undefined) {
+    req.session.routes = [];
+    req.session.routes.push(path);
+    output += `Welcome to http://localhost:${port}`;
+  } else {
+    let previously = `Previously visited:\n  `;
+    previously += req.session.routes.join('\n  ');
+    output += previously;
+    req.session.routes.push(path);
+  }
+  req.generateOutput = output;
+  next();
+};
 
 // Use the express-session module
-app.use(/** ... */);
+app.use(
+  session({
+    store: new session.MemoryStore(),
+    secret: 'shh, this is a secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 86400000,
+    },
+  }),
+);
 
-app.get('/', (req, res) => {
+// For middleware to work, this must be after session is created
+app.use(generateOutput);
+
+// Changed '/' to '*' to catch all paths
+app.get('*', (req, res) => {
   // Add your code here
+  res.status(200);
+  res.set({ 'Content-Type': 'text/plain' });
+  res.send(req.generateOutput);
 });
 
 app.listen(port, () => {
