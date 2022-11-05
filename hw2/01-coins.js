43e679e4f4bc79e5a6c4472b4d2fc37c22c4fb48
JeremiahTilse <91927444+JeremiahTilse@users.noreply.github.com>
2022-11-05 12:28:53 -0700

    Here is my code for Homework 2 (#2)
    * First draft of 01-coins.js
    
    * Reorganization and changed num to cents
    
    * Test cases added
    
    * Fixed typo in comment
    
    * Changed 'ERROR' to 'Error'
    
    * First draft of 02-reverse.js
    
    * FIrst draft of 03-form, js and html
    
    * First draft of 04-api.js
    
    * Removed redundant console.log
    
    * Sorted countries using Intl.Collator
    
    * Updated comments and log
    
    * Updated comments
    
    * Additional comments
    
    * Changed string construction to meet Airbnb guide
    
    * Changed lrt to const where appropriate
    
    * Changed let to const where appropriate
    
    * Changed for-in to forEach to meet Airbnb style
    
    * Fixed comparison operator
    
    * Adding braces for clarity

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -1,7 +1,81 @@
-/** Exercise 01 - Coins **/
+/**
+ * Exercise 01 - Coins *
+ *
+ * @format
+ */
 
 const calculateChange = (input) => {
   // Add your code here
+
+  // Base cases
+  // Zero
+  if (input === 0) return '$0.00 ==> no coins';
+
+  // Not a number ==> Error
+  if (isNaN(input.toString())) return `Error: "${input}" is not a number`;
+
+  // Negative ==> Error
+  if (input < 0) {
+    const negative = 0 - input;
+    return `-$${negative.toString()} ==> Error: the number is negative`;
+  }
+
+  // Larger than 10 ==> Error
+  if (input > 10)
+    return `$${input.toString()} ==> Error: the number is to large`;
+
+  // Not a proper dollar amount ==> Error
+  if (input.toString().length > 4)
+    return `$${input.toString()} ==> Error: the number is not a proper dollar amount`;
+  // End base cases
+
+  // Build the output
+  let output = `$${input.toFixed(2)} ==> `;
+
+  // Convert input to cents for manipulation
+  let cents = input * 100;
+
+  // Separate dollars
+  const dollars = Math.floor(cents / 100);
+  cents = cents % 100;
+  if (dollars > 0) {
+    output = `${output}${dollars.toString()} dollar`;
+    if (dollars > 1) output = `${output}s`;
+    if (cents > 0) output = `${output}, `;
+  }
+
+  // Separate quarters
+  const quarters = Math.floor(cents / 25);
+  cents = cents % 25;
+  if (quarters > 0) {
+    output = `${output}${quarters.toString()} quarter`;
+    if (quarters > 1) output = `${output}s`;
+    if (cents > 0) output = `${output}, `;
+  }
+
+  // Separate dimes
+  const dimes = Math.floor(cents / 10);
+  cents = cents % 10;
+  if (dimes > 0) {
+    output = `${output}${dimes.toString()} dime`;
+    if (dimes > 1) output = `${output}s`;
+    if (cents > 0) output = `${output}, `;
+  }
+
+  // Separate nickles
+  const nickles = Math.floor(cents / 5);
+  cents = cents % 5;
+  if (nickles > 0) {
+    output = `${output}${nickles.toString()} nickle`;
+    if (nickles > 1) output = `${output}s`;
+    if (cents > 0) output = `${output}, `;
+  }
+
+  // The rest in pennies
+  if (cents > 1) output = `${output}${cents.toString()} pennies`;
+  else if (cents > 0) output = `${output}${cents.toString()} penny`;
+
+  return output;
 };
 
 // Sample Test Cases
@@ -13,3 +87,17 @@ console.log(calculateChange(0.16));
 // $0.16 ==> 1 dime, 1 nickel, 1 penny
 console.log(calculateChange(15.11));
 // $15.11 ==> Error: the number is too large
+
+// Additional test cases
+console.log(calculateChange(-9.74));
+// -$9.74 ==> Error: the number is negative
+console.log(calculateChange(4.344));
+// $4.344 ==> Error: the number is not a proper dollar amount
+console.log(calculateChange(1));
+// $1.00 ==> 1 dollars
+console.log(calculateChange(0));
+// $0.00 ==> no coins
+
+const notMoney = 'garbage';
+console.log(calculateChange(notMoney));
+// Error: "garbage" is not a number
--- hw2/03-form.js
+++ hw2/03-form.js
@@ -1,3 +1,59 @@
-/** Exercise 03 - Form **/
+/**
+ * Exercise 03 - Form *
+ *
+ * @format
+ */
 
 // Add your code here
+const form = document.querySelector('form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  const data = {};
+  const all = form.querySelectorAll('input');
+  const feedback = form.querySelector('textarea');
+
+  // Add input values to data object
+  all.forEach((field) => {
+    if (field.type !== 'submit' && field.type !== 'reset') {
+      if (field.type === 'checkbox') {
+        if (field.checked) {
+          data[field.name] = field.value;
+        }
+      } else {
+        if (field.value) data[field.name] = field.value;
+      }
+    }
+  });
+
+  // Add textarea value to data object
+  if (feedback.value) data[feedback.name] = feedback.value;
+
+  const none = 'no submission';
+
+  // Process data for logging
+  if (JSON.stringify(data) === '{}') {
+    console.warn('You must enter some data to submit this form');
+  } else {
+    console.groupCollapsed('========= Form Submission =========');
+
+    data.name ? console.log('Name:', data.name) : console.log('Name:', none);
+
+    data.email
+      ? console.log('Email:', data.email)
+      : console.log('Email:', none);
+
+    data.message
+      ? console.log('Feedback:', data.message)
+      : console.log('Feedback:', 'No feedback was submitted');
+
+    data.signUp
+      ? console.log('Newsletter:', 'Yes, I would like to join the newsletter.')
+      : console.log('Newsletter:', 'No, thank you.');
+
+    console.groupEnd();
+  }
+
+  event.preventDefault();
+}
