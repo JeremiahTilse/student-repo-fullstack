551bca5facfe1e74cb6cd5679c112dca0bd8efbf
Jeremiah <tilse@pdx.edu>
2022-11-05 12:22:32 -0700

    First draft of 03-form

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5001;
@@ -9,8 +11,33 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 // POST request
-app.post('', (req, res) => {
+app.post('/submit', (req, res) => {
   // Add your code here
+  res.status(200);
+  res.set({ 'Content-Type': 'text/html' });
+  const userName = `Name: ${req.body.userName}\n`;
+  const email = `Name: ${req.body.email}`;
+  let message = req.body.message;
+  if (message) {
+    message = `Comments: ${message}`;
+  } else {
+    message = `Comments: n/a`;
+  }
+  let signUp = req.body.signUp;
+  if (signUp) {
+    signUp = `Newsletter: Yes, sign me up for the newsletter.`;
+  } else {
+    signUp = `Newsletter: No, thank you.`;
+  }
+  const output = `
+      <p>
+        ${userName}<br>
+        ${email}<br>
+        ${message}<br>
+        ${signUp}
+      </p>
+    `;
+  res.send(output);
 });
 
 app.listen(port, () => {
